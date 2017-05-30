a28e76e2e9cf665ae0af08bddc50796b361e72cd
senthil <cendhu@gmail.com>
2017-05-30 11:56:25 +0530

    Add hashed and private read-write set to proto
    This CR adds protobuf struct for hashed read/write set and private
    read/write set.
    
    To the existing KVRWSet, i have added a field to store hashed read write
    set of all collections. For a private read, we can store the hash in key
    field but keep the version as is. For private writes, we can store hash
    of key and hash of value but store the IsDelete as is. Hence, reused
    existing KVRead and KVWrite.
    
    For the range query info, it does not make sense to store the hash of
    each field in the RangeQueryInfo struct. Note that we cannot do a range
    query on state DB using hashed startKey and endKey to find phantom
    reads.
    
    Instead of hashed RangeQueryInfo per collection, i have added a string
    field to hold a hash of entire private RangeQueryInfos of that collection.
    During validation, we can compare the hash of entire private RangeQueryInfo
    present in the tempDB with the hash present in the block. I thought of
    doing the same for hashed write-set too as we may not store this value in stateDB.
    However, it might be useful during validation phase (while comparing the
    write set of Ti with read set of Ti+1 -- especially when the private
    write-set is unavailable/not received).
    
    If the peer is configured to move on without private write-set
    (implicit: phantom read check is disabled), we must not use
    RangeQueryInfo. Instead add the read <key, version> to private read
    set and hashed read set.
    
    I have also added PrivateCollectionsKVRWSet to hold private read-write
    set of all collections. For clear differentiation on variable names, i
    have used Reads, HashedReads, and PrivateReads (similarly for the Writes).

--- core/ledger/kvledger/txmgmt/rwsetutil/rwset_proto_util_test.go
+++ core/ledger/kvledger/txmgmt/rwsetutil/rwset_proto_util_test.go
@@ -17,15 +17,18 @@ limitations under the License.
 package rwsetutil
 
 import (
+	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/hyperledger/fabric/common/ledger/testutil"
+	"github.com/hyperledger/fabric/common/util"
 	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/version"
 	"github.com/hyperledger/fabric/protos/ledger/rwset/kvrwset"
 )
 
-func TestTxRWSetMarshalUnmarshal(t *testing.T) {
+func TestTxRWSetMarshalUnmarshalWithoutHashedRWSet(t *testing.T) {
 	txRwSet := &TxRwSet{}
 
 	rqi1 := &kvrwset.RangeQueryInfo{StartKey: "k0", EndKey: "k9", ItrExhausted: true}
@@ -39,21 +42,21 @@ func TestTxRWSetMarshalUnmarshal(t *testing.T) {
 
 	txRwSet.NsRwSets = []*NsRwSet{
 		&NsRwSet{"ns1", &kvrwset.KVRWSet{
-			[]*kvrwset.KVRead{&kvrwset.KVRead{Key: "key1", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
-			[]*kvrwset.RangeQueryInfo{rqi1},
-			[]*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key2", IsDelete: false, Value: []byte("value2")}},
+			Reads:            []*kvrwset.KVRead{&kvrwset.KVRead{Key: "key1", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			RangeQueriesInfo: []*kvrwset.RangeQueryInfo{rqi1},
+			Writes:           []*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key2", IsDelete: false, Value: []byte("value2")}},
 		}},
 
 		&NsRwSet{"ns2", &kvrwset.KVRWSet{
-			[]*kvrwset.KVRead{&kvrwset.KVRead{Key: "key3", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
-			[]*kvrwset.RangeQueryInfo{rqi2},
-			[]*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key3", IsDelete: false, Value: []byte("value3")}},
+			Reads:            []*kvrwset.KVRead{&kvrwset.KVRead{Key: "key3", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			RangeQueriesInfo: []*kvrwset.RangeQueryInfo{rqi2},
+			Writes:           []*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key3", IsDelete: false, Value: []byte("value3")}},
 		}},
 
 		&NsRwSet{"ns3", &kvrwset.KVRWSet{
-			[]*kvrwset.KVRead{&kvrwset.KVRead{Key: "key4", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
-			nil,
-			[]*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key4", IsDelete: false, Value: []byte("value4")}},
+			Reads:            []*kvrwset.KVRead{&kvrwset.KVRead{Key: "key4", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			RangeQueriesInfo: nil,
+			Writes:           []*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key4", IsDelete: false, Value: []byte("value4")}},
 		}},
 	}
 
@@ -62,7 +65,135 @@ func TestTxRWSetMarshalUnmarshal(t *testing.T) {
 	txRwSet1 := &TxRwSet{}
 	testutil.AssertNoError(t, txRwSet1.FromProtoBytes(protoBytes), "")
 	t.Logf("txRwSet=%s, txRwSet1=%s", spew.Sdump(txRwSet), spew.Sdump(txRwSet1))
-	testutil.AssertEquals(t, txRwSet1, txRwSet)
+	testutil.AssertEquals(t, txRwSet, txRwSet1)
+}
+
+func TestTxRWSetMarshalUnmarshalWithHashedRWSet(t *testing.T) {
+	txRwSet := &TxRwSet{}
+
+	rqi1 := &kvrwset.RangeQueryInfo{StartKey: "k0", EndKey: "k9", ItrExhausted: true}
+	rqi1.SetRawReads([]*kvrwset.KVRead{
+		&kvrwset.KVRead{Key: "k1", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}},
+		&kvrwset.KVRead{Key: "k2", Version: &kvrwset.Version{BlockNum: 1, TxNum: 2}},
+	})
+
+	rqi2 := &kvrwset.RangeQueryInfo{StartKey: "k00", EndKey: "k90", ItrExhausted: true}
+	rqi2.SetMerkelSummary(&kvrwset.QueryReadsMerkleSummary{MaxDegree: 5, MaxLevel: 4, MaxLevelHashes: [][]byte{[]byte("Hash-1"), []byte("Hash-2")}})
+
+	privateKey1 := "pKey1"
+	hashedPrivateKey1 := fmt.Sprintf("%x", util.ComputeSHA256([]byte(privateKey1)))
+	privateKey2 := "pKey2"
+	hashedPrivateKey2 := fmt.Sprintf("%x", util.ComputeSHA256([]byte(privateKey2)))
+	privateValue1 := "pValue1"
+	hashedPrivateValue1 := util.ComputeSHA256([]byte(privateValue1))
+
+	txRwSet.NsRwSets = []*NsRwSet{
+
+		&NsRwSet{"ns1", &kvrwset.KVRWSet{
+			[]*kvrwset.KVRead{&kvrwset.KVRead{Key: "key1", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			[]*kvrwset.RangeQueryInfo{rqi1},
+			[]*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key2", IsDelete: false, Value: []byte("value2")}},
+			[]*kvrwset.HashedRWSet{
+				&kvrwset.HashedRWSet{
+					Collection: "ns1-c1",
+					HashedReads: []*kvrwset.KVRead{
+						&kvrwset.KVRead{
+							Key:     hashedPrivateKey1,
+							Version: &kvrwset.Version{BlockNum: 1, TxNum: 1},
+						},
+					},
+					HashedWrites: []*kvrwset.KVWrite{
+						&kvrwset.KVWrite{
+							Key:      hashedPrivateKey2,
+							IsDelete: false,
+							Value:    hashedPrivateValue1,
+						},
+					},
+				},
+			},
+		}},
+
+		&NsRwSet{"ns2", &kvrwset.KVRWSet{
+			[]*kvrwset.KVRead{&kvrwset.KVRead{Key: "key3", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			[]*kvrwset.RangeQueryInfo{rqi2},
+			[]*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key3", IsDelete: false, Value: []byte("value3")}},
+			nil,
+		}},
+
+		&NsRwSet{"ns3", &kvrwset.KVRWSet{
+			[]*kvrwset.KVRead{&kvrwset.KVRead{Key: "key4", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			nil,
+			[]*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key4", IsDelete: false, Value: []byte("value4")}},
+			[]*kvrwset.HashedRWSet{
+				&kvrwset.HashedRWSet{
+					Collection: "ns3-c1",
+					HashedReads: []*kvrwset.KVRead{
+						&kvrwset.KVRead{
+							Key:     hashedPrivateKey1,
+							Version: &kvrwset.Version{BlockNum: 1, TxNum: 1},
+						},
+					},
+					HashedWrites: []*kvrwset.KVWrite{
+						&kvrwset.KVWrite{
+							Key:      hashedPrivateKey2,
+							IsDelete: false,
+							Value:    hashedPrivateValue1,
+						},
+					},
+				},
+			},
+		}},
+	}
+
+	protoBytes, err := txRwSet.ToProtoBytes()
+	testutil.AssertNoError(t, err, "")
+	txRwSet1 := &TxRwSet{}
+	testutil.AssertNoError(t, txRwSet1.FromProtoBytes(protoBytes), "")
+	t.Logf("txRwSet=%s, txRwSet1=%s", spew.Sdump(txRwSet), spew.Sdump(txRwSet1))
+	testutil.AssertEquals(t, txRwSet, txRwSet1)
+}
+
+// Need to ensure that the rwset proto message is compatible with
+// v1.0. This is ensured by unmarshaling the old proto message to the
+// new rwset proto struct.
+func TestTxRWSetMarshalUnmarshalV1BackwardCompatible(t *testing.T) {
+	protoBytes, err := ioutil.ReadFile("./rwsetV1ProtoBytes")
+	testutil.AssertNoError(t, err, "")
+	txRwSet1 := &TxRwSet{}
+	testutil.AssertNoError(t, txRwSet1.FromProtoBytes(protoBytes), "")
+
+	txRwSet := &TxRwSet{}
+
+	rqi1 := &kvrwset.RangeQueryInfo{StartKey: "k0", EndKey: "k9", ItrExhausted: true}
+	rqi1.SetRawReads([]*kvrwset.KVRead{
+		&kvrwset.KVRead{Key: "k1", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}},
+		&kvrwset.KVRead{Key: "k2", Version: &kvrwset.Version{BlockNum: 1, TxNum: 2}},
+	})
+
+	rqi2 := &kvrwset.RangeQueryInfo{StartKey: "k00", EndKey: "k90", ItrExhausted: true}
+	rqi2.SetMerkelSummary(&kvrwset.QueryReadsMerkleSummary{MaxDegree: 5, MaxLevel: 4, MaxLevelHashes: [][]byte{[]byte("Hash-1"), []byte("Hash-2")}})
+
+	txRwSet.NsRwSets = []*NsRwSet{
+		&NsRwSet{"ns1", &kvrwset.KVRWSet{
+			Reads:            []*kvrwset.KVRead{&kvrwset.KVRead{Key: "key1", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			RangeQueriesInfo: []*kvrwset.RangeQueryInfo{rqi1},
+			Writes:           []*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key2", IsDelete: false, Value: []byte("value2")}},
+		}},
+
+		&NsRwSet{"ns2", &kvrwset.KVRWSet{
+			Reads:            []*kvrwset.KVRead{&kvrwset.KVRead{Key: "key3", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			RangeQueriesInfo: []*kvrwset.RangeQueryInfo{rqi2},
+			Writes:           []*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key3", IsDelete: false, Value: []byte("value3")}},
+		}},
+
+		&NsRwSet{"ns3", &kvrwset.KVRWSet{
+			Reads:            []*kvrwset.KVRead{&kvrwset.KVRead{Key: "key4", Version: &kvrwset.Version{BlockNum: 1, TxNum: 1}}},
+			RangeQueriesInfo: nil,
+			Writes:           []*kvrwset.KVWrite{&kvrwset.KVWrite{Key: "key4", IsDelete: false, Value: []byte("value4")}},
+		}},
+	}
+	t.Logf("txRwSet=%s, txRwSet1=%s", spew.Sdump(txRwSet), spew.Sdump(txRwSet1))
+	testutil.AssertEquals(t, txRwSet, txRwSet1)
 }
 
 func TestVersionConversion(t *testing.T) {
