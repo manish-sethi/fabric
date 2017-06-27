8b47be8742cd2804a8a7804f9f229e5bb9d12ac9
root <cendhu@gmail.com>
2017-06-27 16:35:03 +0530

    Transient Store
    This CR adds transient store for storing/retrieving/purging the
    private read write set generated as a result of a transaction
    simulation.
    
    Unit test provides 100% coverage.

--- core/ledger/kvledger/pvtrwstorage/transient_store_helper.go
+++ core/ledger/kvledger/pvtrwstorage/transient_store_helper.go
@@ -22,18 +22,11 @@ import (
 	"github.com/hyperledger/fabric/common/ledger/util"
 )
 
-type count uint8
-
-const (
-	NUM_ATTRIBUTES_IN_PRWSET_KEY      count = 4 //namespace~txId~endorserId~endorsementBlkHt
-	NUM_ATTRIBUTES_IN_PURGE_INDEX_KEY count = 4 //namespace~endorsementBlkHt~txId~endorserId
-)
-
 var compositeKeySep = []byte{0x00}
 
-// CreateCompositeKeyForPRWSet creates a key for storing private read-write set
+// createCompositeKeyForPRWSet creates a key for storing private read-write set
 // in the transient store. The structure of the key is namespace~transactionId~endorserId.
-func CreateCompositeKeyForPRWSet(ns string, txId string, endorserId string, endorsementBlkHt uint64) []byte {
+func createCompositeKeyForPRWSet(ns string, txId string, endorserId string, endorsementBlkHt uint64) []byte {
 	var compositeKey []byte
 
 	compositeKey = append(compositeKey, []byte(ns)...)
@@ -47,10 +40,10 @@ func CreateCompositeKeyForPRWSet(ns string, txId string, endorserId string, endo
 	return compositeKey
 }
 
-// CreateCompositeKeyForPurgeIndex creates a key to index private read-write set based on
+// createCompositeKeyForPurgeIndex creates a key to index private read-write set based on
 // endorsement block height such that purge based on block height can be achieved. The structure
 // of the key is namespace~endorsementBlkHt~txId~endorserId.
-func CreateCompositeKeyForPurgeIndex(ns string, endorsementBlkHt uint64, txId string, endorserId string) []byte {
+func createCompositeKeyForPurgeIndex(ns string, endorsementBlkHt uint64, txId string, endorserId string) []byte {
 	var compositeKey []byte
 
 	compositeKey = append(compositeKey, []byte(ns)...)
@@ -64,8 +57,8 @@ func CreateCompositeKeyForPurgeIndex(ns string, endorsementBlkHt uint64, txId st
 	return compositeKey
 }
 
-// SplitCompositeKey splits the compositeKey into splitCount number of attributes.
-func SplitCompositeKeyOfPRWSet(compositeKey []byte) (endorserId string, endorsementBlkHt uint64) {
+// splitCompositeKeyOfPRWSet splits the compositeKey into endorserId and endorsementBlkHt.
+func splitCompositeKeyOfPRWSet(compositeKey []byte) (endorserId string, endorsementBlkHt uint64) {
 	splits := bytes.Split(compositeKey, compositeKeySep)
 	// splits[0] is the namesapce & splits[1] is the txId: Ignored as they are not required currently.
 	endorserId = string(splits[2])
@@ -73,7 +66,8 @@ func SplitCompositeKeyOfPRWSet(compositeKey []byte) (endorserId string, endorsem
 	return endorserId, endorsementBlkHt
 }
 
-func SplitCompositeKeyOfPurgeIndex(compositeKey []byte) (txId string, endorserId string, endorsementBlkHt uint64) {
+// splitCompositeKeyOfPurgeIndex splits the compositeKey into txId, endorserId and endorsementBlkHt.
+func splitCompositeKeyOfPurgeIndex(compositeKey []byte) (txId string, endorserId string, endorsementBlkHt uint64) {
 	splits := bytes.Split(compositeKey, compositeKeySep)
 	// splits[0] is the namesapce: Ignored as it is not required currently.
 	endorsementBlkHt, _ = util.DecodeOrderPreservingVarUint64(splits[1])
@@ -82,29 +76,43 @@ func SplitCompositeKeyOfPurgeIndex(compositeKey []byte) (txId string, endorserId
 	return txId, endorserId, endorsementBlkHt
 }
 
-func CreatePartialCompositeKeyForPRWSet(ns string, txId string, endKey bool) []byte {
-	var partialCompositeKey []byte
-
-	partialCompositeKey = append(partialCompositeKey, []byte(ns)...)
-	partialCompositeKey = append(partialCompositeKey, compositeKeySep...)
-	partialCompositeKey = append(partialCompositeKey, []byte(txId)...)
-
-	if endKey {
-		partialCompositeKey = append(partialCompositeKey, []byte{0xff}...)
-	}
-	return partialCompositeKey
+// createTxIdRangeStartKey returns a startKey to do a range query on transient store using txId
+func createTxIdRangeStartKey(prefix string, txId string) []byte {
+	var startKey []byte
+	startKey = append(startKey, []byte(prefix)...)
+	startKey = append(startKey, compositeKeySep...)
+	startKey = append(startKey, []byte(txId)...)
+	startKey = append(startKey, compositeKeySep...)
+	return startKey
 }
 
-func CreatePartialCompositeKeyForPurgeIndex(ns string, endorsementBlkHt uint64, endKey bool) []byte {
-	var partialCompositeKey []byte
+// createTxIdRangeStartKey returns a endKey to do a range query on transient store using txId
+func createTxIdRangeEndKey(prefix string, txId string) []byte {
+	var endKey []byte
+	endKey = append(endKey, []byte(prefix)...)
+	endKey = append(endKey, compositeKeySep...)
+	endKey = append(endKey, []byte(txId)...)
+	endKey = append(endKey, []byte{0xff}...)
+	return endKey
+}
 
-	partialCompositeKey = append(partialCompositeKey, []byte(ns)...)
-	partialCompositeKey = append(partialCompositeKey, compositeKeySep...)
-	partialCompositeKey = append(partialCompositeKey, util.EncodeOrderPreservingVarUint64(endorsementBlkHt)...)
+// createEndorsementBlkHtRangeStartKey returns a startKey to do a range query on index stored in transient store
+// using endorsementBlkHt
+func createEndorsementBlkHtRangeStartKey(prefix string, endorsementBlkHt uint64) []byte {
+	var startKey []byte
+	startKey = append(startKey, []byte(prefix)...)
+	startKey = append(startKey, compositeKeySep...)
+	startKey = append(startKey, util.EncodeOrderPreservingVarUint64(endorsementBlkHt)...)
+	return startKey
+}
 
-	if endKey {
-		partialCompositeKey = append(partialCompositeKey, []byte{0xff}...)
-	}
-	return partialCompositeKey
-
+// createEndorsementBlkHtRangeStartKey returns a endKey to do a range query on index stored in transient store
+// using endorsementBlkHt
+func createEndorsementBlkHtRangeEndKey(prefix string, endorsementBlkHt uint64) []byte {
+	var endKey []byte
+	endKey = append(endKey, []byte(prefix)...)
+	endKey = append(endKey, compositeKeySep...)
+	endKey = append(endKey, util.EncodeOrderPreservingVarUint64(endorsementBlkHt)...)
+	endKey = append(endKey, []byte{0xff}...)
+	return endKey
 }
--- core/ledger/kvledger/pvtrwstorage/transient_store_test.go
+++ core/ledger/kvledger/pvtrwstorage/transient_store_test.go
@@ -19,49 +19,178 @@ package pvtrwstorage
 import (
 	commonledger "github.com/hyperledger/fabric/common/ledger"
 	"github.com/hyperledger/fabric/core/ledger"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
-func TestTransientStore(t *testing.T) {
+func TestMain(m *testing.M) {
+	viper.Set("peer.fileSystemPath", "/tmp/fabric/ledgertests/kvledger/")
+	os.Exit(m.Run())
+}
+
+func TestTransientStorePersistAndRetrieve(t *testing.T) {
 	env := NewTestTransientStoreEnv(t)
 	assert := assert.New(t)
 
 	txId := "txId-1"
 
+	// Create private simulation results for txId-1
 	var endorsersResults []*ledger.EndorserPrivateSimulationResults
+
+	// Results produced by endorser 1
 	endorser0SimulationResults := &ledger.EndorserPrivateSimulationResults{
-		EndorserId:               "endorser1",
+		EndorserId:               "endorser0",
 		EndorsementBlockHeight:   10,
 		PrivateSimulationResults: []byte("results"),
 	}
 	endorsersResults = append(endorsersResults, endorser0SimulationResults)
 
+	// Results produced by endorser 2
 	endorser1SimulationResults := &ledger.EndorserPrivateSimulationResults{
-		EndorserId:               "endorser2",
+		EndorserId:               "endorser1",
 		EndorsementBlockHeight:   10,
 		PrivateSimulationResults: []byte("results"),
 	}
 	endorsersResults = append(endorsersResults, endorser1SimulationResults)
 
+	// Persist simulation results into transient store
 	var err error
-	err = env.testTransientStore.Persist(txId, endorsersResults[0].EndorserId,
-		endorsersResults[0].EndorsementBlockHeight, endorsersResults[0].PrivateSimulationResults)
-	assert.NoError(err)
+	for i := 0; i < 2; i++ {
+		err = env.testTransientStore.Persist(txId, endorsersResults[i].EndorserId,
+			endorsersResults[i].EndorsementBlockHeight, endorsersResults[i].PrivateSimulationResults)
+		assert.NoError(err)
+	}
 
-	err = env.testTransientStore.Persist(txId, endorsersResults[1].EndorserId,
-		endorsersResults[1].EndorsementBlockHeight, endorsersResults[1].PrivateSimulationResults)
-	assert.NoError(err)
-
+	// Retrieve simulation results of txId-1 from transient store
 	var iter commonledger.ResultsIterator
 	iter, err = env.testTransientStore.GetTxPrivateRWSetByTxId(txId)
 	assert.NoError(err)
 
 	var result commonledger.QueryResult
 	var actualEndorsersResults []*ledger.EndorserPrivateSimulationResults
-	for result, err = iter.Next(); result != nil; {
+	for true {
+		result, err = iter.Next()
+		assert.NoError(err)
+		if result == nil {
+			break
+		}
 		actualEndorsersResults = append(actualEndorsersResults, result.(*ledger.EndorserPrivateSimulationResults))
 	}
-	assert.NoError(err)
+	iter.Close()
 	assert.Equal(endorsersResults, actualEndorsersResults)
+
+	env.cleanup()
+}
+
+func TestTransientStorePurge(t *testing.T) {
+	env := NewTestTransientStoreEnv(t)
+	assert := assert.New(t)
+
+	txId := "txId-1"
+
+	// Create private simulation results for txId-1
+	var endorsersResults []*ledger.EndorserPrivateSimulationResults
+
+	// Results produced by endorser 1
+	endorser0SimulationResults := &ledger.EndorserPrivateSimulationResults{
+		EndorserId:               "endorser0",
+		EndorsementBlockHeight:   10,
+		PrivateSimulationResults: []byte("results"),
+	}
+	endorsersResults = append(endorsersResults, endorser0SimulationResults)
+
+	// Results produced by endorser 2
+	endorser1SimulationResults := &ledger.EndorserPrivateSimulationResults{
+		EndorserId:               "endorser1",
+		EndorsementBlockHeight:   11,
+		PrivateSimulationResults: []byte("results"),
+	}
+	endorsersResults = append(endorsersResults, endorser1SimulationResults)
+
+	// Results produced by endorser 3
+	endorser2SimulationResults := &ledger.EndorserPrivateSimulationResults{
+		EndorserId:               "endorser2",
+		EndorsementBlockHeight:   12,
+		PrivateSimulationResults: []byte("results"),
+	}
+	endorsersResults = append(endorsersResults, endorser2SimulationResults)
+
+	// Results produced by endorser 3
+	endorser3SimulationResults := &ledger.EndorserPrivateSimulationResults{
+		EndorserId:               "endorser3",
+		EndorsementBlockHeight:   12,
+		PrivateSimulationResults: []byte("results"),
+	}
+	endorsersResults = append(endorsersResults, endorser3SimulationResults)
+
+	// Results produced by endorser 3
+	endorser4SimulationResults := &ledger.EndorserPrivateSimulationResults{
+		EndorserId:               "endorser4",
+		EndorsementBlockHeight:   13,
+		PrivateSimulationResults: []byte("results"),
+	}
+	endorsersResults = append(endorsersResults, endorser4SimulationResults)
+
+	// Persist simulation results into transient store
+	var err error
+	for i := 0; i < 5; i++ {
+		err = env.testTransientStore.Persist(txId, endorsersResults[i].EndorserId,
+			endorsersResults[i].EndorsementBlockHeight, endorsersResults[i].PrivateSimulationResults)
+		assert.NoError(err)
+	}
+
+	// Retain results generate at block height greater than or equal to 12
+	minEndorsementBlkHtToRetain := uint64(12)
+	err = env.testTransientStore.Purge(minEndorsementBlkHtToRetain)
+	assert.NoError(err)
+
+	// Retrieve simulation results of txId-1 from transient store
+	var iter commonledger.ResultsIterator
+	iter, err = env.testTransientStore.GetTxPrivateRWSetByTxId(txId)
+	assert.NoError(err)
+
+	// Expected results for txId-1
+	var expectedEndorsersResults []*ledger.EndorserPrivateSimulationResults
+	expectedEndorsersResults = append(expectedEndorsersResults, endorser2SimulationResults) //endorsed at height 12
+	expectedEndorsersResults = append(expectedEndorsersResults, endorser3SimulationResults) //endorsed at height 12
+	expectedEndorsersResults = append(expectedEndorsersResults, endorser4SimulationResults) //endorsed at height 13
+
+	// Check whether actual results and expected results are same
+	var result commonledger.QueryResult
+	var actualEndorsersResults []*ledger.EndorserPrivateSimulationResults
+	for true {
+		result, err = iter.Next()
+		assert.NoError(err)
+		if result == nil {
+			break
+		}
+		actualEndorsersResults = append(actualEndorsersResults, result.(*ledger.EndorserPrivateSimulationResults))
+	}
+	iter.Close()
+	assert.Equal(expectedEndorsersResults, actualEndorsersResults)
+
+	// Get the minimum retained endorsement block height
+	var actualMinEndorsementBlkHt uint64
+	actualMinEndorsementBlkHt, err = env.testTransientStore.GetMinEndorsementBlkHt()
+	assert.NoError(err)
+	assert.Equal(minEndorsementBlkHtToRetain, actualMinEndorsementBlkHt)
+
+	// Retain results generate at block height greater than or equal to 15
+	minEndorsementBlkHtToRetain = uint64(15)
+	err = env.testTransientStore.Purge(minEndorsementBlkHtToRetain)
+	assert.NoError(err)
+
+	// There should be no entries in the transient store
+	actualMinEndorsementBlkHt, err = env.testTransientStore.GetMinEndorsementBlkHt()
+	assert.Equal(err, ErrTransientStoreEmpty)
+
+	// Retain results generate at block height greater than or equal to 15
+	minEndorsementBlkHtToRetain = uint64(15)
+	err = env.testTransientStore.Purge(minEndorsementBlkHtToRetain)
+	// Should not return any error
+	assert.NoError(err)
+
+	env.cleanup()
 }
